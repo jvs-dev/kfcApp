f667c35d0c9be0c5f6815ddeedd9fc9e24f3fc71
joão vitor <shepherdcom12@gmail.com>
2024-06-28 03:39:58 -0300

    fix image bug

--- src/scripts/alterImage.js
+++ src/scripts/alterImage.js
@@ -1,4 +1,3 @@
-let image = document.getElementById("mainImage")
 let nextImg = document.getElementById("nextImg")
 let previousImg = document.getElementById("previousImg")
 let imageIndex = 1
@@ -16,19 +15,32 @@ function alterImage(n) {
     if (n > 0) {
         if (imageIndex == numberOfImages) {
             imageIndex = 1
-            image.src = `src/assets/mainImage${imageIndex}.png`
+            disableAllImages()
+            document.getElementById(`mainImage${imageIndex}`).style.display = "unset"
         } else {
             imageIndex = imageIndex + 1
-            image.src = `src/assets/mainImage${imageIndex}.png`
+            disableAllImages()
+            document.getElementById(`mainImage${imageIndex}`).style.display = "unset"
         }
     } else {
         if (imageIndex == 1) {
             imageIndex = numberOfImages
-            image.src = `src/assets/mainImage${imageIndex}.png`
+            disableAllImages()
+            document.getElementById(`mainImage${imageIndex}`).style.display = "unset"
         } else {
             imageIndex = imageIndex - 1
-            image.src = `src/assets/mainImage${imageIndex}.png`
+            disableAllImages()
+            document.getElementById(`mainImage${imageIndex}`).style.display = "unset"
         }
     }
 }
 
+function disableAllImages() {
+    document.getElementById(`mainImage1`).style.display = "none"
+    document.getElementById(`mainImage2`).style.display = "none"
+    document.getElementById(`mainImage3`).style.display = "none"
+    document.getElementById(`mainImage4`).style.display = "none"
+    document.querySelectorAll(".main__img").forEach(element => {
+        element.style.animation = "none"
+    });    
+}
